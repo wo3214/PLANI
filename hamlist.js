c3709b809d321eb17cbff0d04b5e85b01416d86d
wo3214 <wo32144@naver.com>
2022-12-13 10:27:31 +0900

    123

--- hamlist.js
+++ hamlist.js
@@ -1,12 +1,54 @@
-const heBtn = document.querySelector(".header_menu_btn");
-const haMenu = document.querySelector(".ham_menu");
-const haText = document.querySelector(".ham_text");
-const haList = document.querySelector(".ham_list");
-const haButn = document.querySelector(".ham_button");
+$("body").prepend("<header>");
+$("body").append("<footer>");
 
-heBtn.onclick = function () {
-  haMenu.classList.toggle("heon");
-  haText.classList.toggle("ton");
-  haList.classList.toggle("hlon");
-  haButn.classList.toggle("bon");
-};
+$("header").load("inc.html header>nav", head);
+$("footer").load("inc.html footer>div", foot);
+
+function head() {
+  let header = document.querySelector("header");
+  let headerHeight = header.offsetHeight;
+
+  window.onscroll = function () {
+    let windowTop = window.scrollY;
+    if (windowTop >= headerHeight) {
+      header.style.zIndex = "999";
+      header.classList.add("drop");
+    } else {
+      header.classList.remove("drop");
+    }
+  };
+
+  const heBtn = document.querySelector(".header_menu_btn");
+  const haMenu = document.querySelector(".ham_menu");
+  const haText = document.querySelector(".ham_text");
+  const haList = document.querySelector(".ham_list");
+  const haButn = document.querySelector(".ham_button");
+
+  heBtn.onclick = function () {
+    haMenu.classList.toggle("heon");
+    haText.classList.toggle("ton");
+    haList.classList.toggle("hlon");
+    haButn.classList.toggle("bon");
+  };
+
+  let idx = localStorage.idx;
+  $(".header_menu_bar li").removeClass("active");
+
+  $(".header_menu_bar li").eq(idx).addClass("active");
+
+  $(".header_menu_bar li").on("click", function () {
+    idx = $(this).index();
+    localStorage.idx = idx;
+  });
+}
+
+function foot() {
+  const ulBtn = document.querySelector(".text_icon");
+  const ulList = document.querySelector(".button_list");
+  const iCon = document.querySelector(".text_icon i");
+
+  ulBtn.onclick = function () {
+    ulList.classList.toggle("on");
+    iCon.classList.toggle("ion");
+  };
+}
